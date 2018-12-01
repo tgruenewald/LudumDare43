a09f8d7a6700b392d8b44a4a04e836c9e3d0b286
Jack Campbell <jackcampbell@acm.org>
2018-12-01 13:04:42 -0700

    adding first pass of actions ui

---
+++ upsystem/Assets/Scripts/UI_ShipTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_ShipTest : MonoBehaviour {
+
+    public ActionSelector mActionSelector;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnMouseUp()
+    {
+        mActionSelector.Toggle();
+    }
+}
