65b244ba271813629775508b626b5afabbd8375f
Jack Campbell <jackcampbell@acm.org>
2018-12-01 16:32:00 -0700

    fixing virtual method

--- upsystem/Assets/Scripts/PassengerShip.cs
+++ upsystem/Assets/Scripts/PassengerShip.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 class PassengerShip : Ship
 {
-    public void initialize(int StartingCrew, int StartingSupply, int StartingFuel, bool Damaged)
+    public void Initialize(int StartingCrew, int StartingSupply, int StartingFuel, bool Damaged)
     {
         base.Initialize(StartingCrew, StartingSupply, StartingFuel, !Damaged, ShipType.Passenger, 8, 2, 2);
     }
