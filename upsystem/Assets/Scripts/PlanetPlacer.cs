c108f0bc9b6c701e55873b063e2c47023c68db3b
Brandio <strochba@mail.uc.edu>
2018-12-02 01:54:40 -0500

    start planet placer and prefab buttons

---
+++ upsystem/Assets/Scripts/PlanetPlacer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetPlacer : MonoBehaviour {
+    public float maxX = 1.1F;
+    public float maxY = 1.1F;
+    public Transform planet;
+    int prvIndex = -1;
+    public List<Sprite> planetSprites = new List<Sprite>();
+    
+    void PlacePlanet()
+    {
+        planet.transform.position = new Vector3(Random.Range(0, maxX), Random.Range(0, maxY), 0);
+        int index = -1;
+        bool foundIndex = false;
+        while(!foundIndex)
+        {
+            index = Random.Range(0, planetSprites.Count);
+            if(index != prvIndex)
+            {
+                prvIndex = index;
+                foundIndex = true;
+                SpriteRenderer renderer = planet.gameObject.GetComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+                renderer.sprite = planetSprites[index];
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        PlacePlanet();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
